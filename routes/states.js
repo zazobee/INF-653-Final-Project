e2794ffb6f8324c3f5528af78f2efbc612d94536
zazobee <zazaboutdara@yahoo.com>
2026-05-13 23:18:47 -0500

    test

--- routes/states.js
+++ routes/states.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const statesController = require('../controllers/statesController');
-const verifyStates = require('../middleware/verifyStates');
+const statesController = require('../controllers/statesController.js');
 
-router.get('/', statesController.getAllStates);
-router.get('/:state', verifyStates, statesController.getState);
-router.get('/:state/funfact', verifyStates, statesController.getFunFact);
-router.get('/:state/capital', verifyStates, statesController.getCapital);
-router.get('/:state/nickname', verifyStates, statesController.getNickname);
-router.get('/:state/population', verifyStates, statesController.getPopulation);
-router.get('/:state/admission', verifyStates, statesController.getAdmission);
-router.post('/:state/funfact', verifyStates, statesController.addFunFact);
-router.patch('/:state/funfact', verifyStates, statesController.updateFunFact);
-router.delete('/:state/funfact', verifyStates, statesController.deleteFunFact);
+router.get('/', getAllStates);
+router.get('/:state', statesController.getState);
+router.get('/:state/funfact', statesController.getFunFact);
+router.get('/:state/capital', statesController.getCapital);
+router.get('/:state/nickname', statesController.getNickname);
+router.get('/:state/population', statesController.getPopulation);
+router.get('/:state/admission', statesController.getAdmission);
+router.post('/:state/funfact', statesController.addFunFact);
+router.patch('/:state/funfact', statesController.updateFunFact);
+router.delete('/:state/funfact', statesController.deleteFunFact);
 
 module.exports = router;
